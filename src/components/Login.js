827601f1dda1b8c98ff1f43279c0734d17054503
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-02 14:46:55 +0800

    updated signup form style

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,36 +4,45 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 const Login = () => {
 	return (
 		<Container>
-			<Form>
-				<h2>Log In</h2>
-				<p>使用Google 或 Facebook登入</p>
-				<Container>
-					<Row className='text-center'>
-						<Col md='6'>
-							<Button variant='primary'>Facebook</Button>
-						</Col>
-						<Col md='6'>
-							<Button variant='danger'>Google</Button>
-						</Col>
-					</Row>
-				</Container>
-				<div className='or-seperator'>
-					<b>Or</b>
-				</div>
-				<Form.Group className='mb-3'>
-					<Form.Label>Email</Form.Label>
-					<Form.Control type='email' placeholder='Enter email' />
-				</Form.Group>
-				<Form.Group className='mb-3'>
-					<Form.Label>Password</Form.Label>
-					<Form.Control type='password' placeholder='Password' />
-				</Form.Group>
-				<Button variant='success'>Sign In</Button>
-			</Form>
+			<Card className='p-5 mt-5'>
+				<Form>
+					<h2>Log In</h2>
+					<p>使用Google 或 Facebook登入</p>
+					<Container>
+						<Row className='text-center'>
+							<Col md='12'>
+								<Button variant='primary' size='lg' block>
+									Facebook
+								</Button>
+							</Col>
+							<Col md='12' className='mt-5'>
+								<Button variant='danger' size='lg' block>
+									Google
+								</Button>
+							</Col>
+						</Row>
+					</Container>
+					<div className='or-seperator'>
+						<b>Or</b>
+					</div>
+					<Form.Group className='mb-3'>
+						<Form.Label>Email</Form.Label>
+						<Form.Control type='email' placeholder='Enter email' />
+					</Form.Group>
+					<Form.Group className='mb-3'>
+						<Form.Label>Password</Form.Label>
+						<Form.Control type='password' placeholder='Password' />
+					</Form.Group>
+					<Button variant='success' size='lg' block>
+						Sign In
+					</Button>
+				</Form>
+			</Card>
 		</Container>
 	);
 };
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,22 +2,25 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const SignUp = () => {
 	return (
 		<Container>
-			<Form>
-				<h2>Sign Up Form</h2>
-				<Form.Group className='mb-3'>
-					<Form.Label>Email</Form.Label>
-					<Form.Control type='email' placeholder='Enter email' />
-				</Form.Group>
-				<Form.Group className='mb-3'>
-					<Form.Label>Password</Form.Label>
-					<Form.Control type='password' placeholder='Password' />
-				</Form.Group>
-				<Button variant='primary'>Sign Up</Button>
-			</Form>
+			<Card>
+				<Form className='text-center'>
+					<h2>Sign Up Form</h2>
+					<Form.Group className='mb-3'>
+						<Form.Label>Email</Form.Label>
+						<Form.Control type='email' placeholder='Enter email' />
+					</Form.Group>
+					<Form.Group className='mb-3'>
+						<Form.Label>Password</Form.Label>
+						<Form.Control type='password' placeholder='Password' />
+					</Form.Group>
+					<Button variant='primary'>Sign Up</Button>
+				</Form>
+			</Card>
 		</Container>
 	);
 };
