63d46d281169775200eaaa9be9953b1ffad68e2d
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-02 11:56:33 +0800

    built Landing Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TopBar from './components/TopBar';
+import Landing from './components/Landing';
 
 function App() {
 	return (
 		<div>
 			<TopBar />
+			<Landing />
 		</div>
 	);
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+const Landing = () => {
+	return (
+		<>
+			<Jumbotron fluid style={{ height: '50vh' }}>
+				<Container>
+					<Row>
+						<Col md={{ span: 6, offset: 3 }}>
+							<h1 className='text-center'>A-station</h1>
+						</Col>
+					</Row>
+					<Row>
+						<Col md={{ span: 6, offset: 3 }}>
+							<h3 className='text-center'>A-station</h3>
+						</Col>
+					</Row>
+					<Row>
+						<Col md={{ span: 6, offset: 3 }}>
+							<div className='text-center mt-5'>
+								<Button
+									variant='primary'
+									className='mr-4'
+									size='lg'
+								>
+									開始學習
+								</Button>
+								<Button variant='secondary' size='lg'>
+									了解更多
+								</Button>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</Jumbotron>
+			<Container>
+				<Row>
+					<Col md='4'>
+						<h2>線上DSE練習</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Ex ut inventore facere sit placeat est eius
+							sint dolore ducimus id soluta necessitatibus cum,
+							nihil, ullam corporis ad libero doloremque tempore.
+						</p>
+					</Col>
+					<Col md='4'>
+						<h2>線上DSE練習</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Ex ut inventore facere sit placeat est eius
+							sint dolore ducimus id soluta necessitatibus cum,
+							nihil, ullam corporis ad libero doloremque tempore.
+						</p>
+					</Col>
+					<Col md='4'>
+						<h2>線上DSE練習</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Ex ut inventore facere sit placeat est eius
+							sint dolore ducimus id soluta necessitatibus cum,
+							nihil, ullam corporis ad libero doloremque tempore.
+						</p>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
+};
+
+export default Landing;
