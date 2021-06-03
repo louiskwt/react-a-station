53a8af34ac2ce43d2bb3552752d41d01d9479078
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-03 10:06:08 +0800

    cleaned up Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,20 +27,6 @@ const Login = () => {
 							</Col>
 						</Row>
 					</Container>
-					<div className='or-seperator'>
-						<b>Or</b>
-					</div>
-					<Form.Group className='mb-3'>
-						<Form.Label>Email</Form.Label>
-						<Form.Control type='email' placeholder='Enter email' />
-					</Form.Group>
-					<Form.Group className='mb-3'>
-						<Form.Label>Password</Form.Label>
-						<Form.Control type='password' placeholder='Password' />
-					</Form.Group>
-					<Button variant='success' size='lg' block>
-						Sign In
-					</Button>
 				</Form>
 			</Card>
 		</Container>
