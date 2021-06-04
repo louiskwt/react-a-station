59a988f0f04bb3d93591b7a2fee974ca40c84307
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-04 09:44:34 +0800

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBU5ZW_WN6IP-4cKgZLXNEOeIoRsxAIJWg',
+	authDomain: 'a-station.firebaseapp.com',
+	projectId: 'a-station',
+	storageBucket: 'a-station.appspot.com',
+	messagingSenderId: '985513984024',
+	appId: '1:985513984024:web:f4e4975423c3c57f05036d'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp, auth };
