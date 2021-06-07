8d99c0629b28738e809422b8ce6d6d0d78740236
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-07 14:12:57 +0800

    built basic layout for exercise page

--- src/pages/ReadingEx.js
+++ src/pages/ReadingEx.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import { useReading } from '../hooks/useFirestore';
 
 const ReadingEx = () => {
 	const { id } = useParams();
 	console.log(id);
-	const questions = useReading(id);
-	console.log(questions);
+	const { questions } = useReading(id);
+
 	return (
-		<div>
+		<div className='container'>
 			<h1>Reading Ex</h1>
+			<Row>
+				<Col md={8}>
+					<div className='container bg-light'>
+						<h4>{questions.title}</h4>
+					</div>
+				</Col>
+				<Col md={4}>Q&A</Col>
+			</Row>
+			<Row>
+				<div className='fixed-bottom'>Panel</div>
+			</Row>
 		</div>
 	);
 };
